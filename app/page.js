eed09b4ab57d54b496d701068f6f1cc413d82eeb
Regis066 <mugisharegis72@gmail.com>
2023-05-13 22:26:17 +0200

    added new things

--- app/page.js
+++ app/page.js
@@ -6,12 +6,12 @@ const Home = () => {
       Hello Banner
 
 
-      <div>
+      <div className='products-heading'>
         <h2>Beset Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
-      <div>
+      <div className='product-container'>
       {['Product 1', 'Product 2'].map((product)=> product)}
       </div>
 
